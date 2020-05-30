2b464298b5dd34369fdf109455bf08fc5a8a822b
jed hammi <jed.hammi@esprit.tn>
2020-05-30 03:52:30 +0200

    rebuild

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ provider "azurerm" {
     # The "feature" block is required for AzureRM provider 2.x. 
     # If you are using version 1.x, the "features" block is not allowed.
     version = "~>2.0"
-    #subscription_id ="cfac6a63-b3f3-4682-bd35-b3b5d9424b2d"
-    #client_id       ="f6d456fa-bf84-436a-8231-147b904c6ca1"
-    #client_secret   ="3b762305-6680-4fac-a03a-8689351b0a93"
-    #tenant_id       = "27edc1b2-a527-4f5d-82d5-84b7dc31cdc6"
 
 
 
